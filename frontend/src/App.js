5d5a02d674172ca6e77f301cc93187aa0f2a6205
Htalharshit <harshitagarwalhtalhtal@gmail.com>
2024-07-05 01:07:07 +0530

    Routes Created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,29 @@
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Shop from "./pages/Shop";
+import ShopCategory from "./pages/ShopCategory";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import LoginForm from "./pages/LoginForm";
 
 function App() {
   return (
     <div className="w-[95vw] m-auto">
-      <Navbar/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="mens" />} />
+          <Route path="/womens" element={<ShopCategory category="womens" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import logo from "../assets/logo.png"
+import { Link } from 'react-router-dom';
 
 
 
@@ -13,14 +14,14 @@ const Navbar = () => {
                         <img src={logo} alt="" className='w-72' />
                     </div>
                     <ul className='flex space-x-10'>
-                        <li className={`${activeState === "shop" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("shop")}>Shop</li>
-                        <li className={`${activeState === "men" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("men")}>Men</li>
-                        <li className={`${activeState === "women" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("women")}>Women</li>
-                        <li className={`${activeState === "kids" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("kids")}>Kids</li>
+                        <li className={`${activeState === "shop" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("shop")}><Link to="/">Shop</Link></li>
+                        <li className={`${activeState === "men" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("men")}><Link to="/mens">Men</Link></li>
+                        <li className={`${activeState === "women" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("women")}><Link to="/womens">Women</Link></li>
+                        <li className={`${activeState === "kids" ? "py-2 border-b-4 border-solid border-blue-600" : ""} cursor-pointer`} onClick={() => setActiveState("kids")}><Link to="/kids">Kids</Link></li>
                     </ul>
                     <div className='space-x-10 '>
-                        <button className='px-5 py-2 font-bold bg-blue-600 hover:bg-white text-white hover:text-blue-600 border-2 border-solid border-white hover:border-blue-600 rounded-full cursor-pointer'>Login</button>
-                        <i className="fa-solid fa-cart-shopping px-3 py-1 text-xl text-blue-600 hover:text-blue-400 cursor-pointer"></i>
+                        <Link to="/login"><button className='px-5 py-2 font-bold bg-blue-600 hover:bg-white text-white hover:text-blue-600 border-2 border-solid border-white hover:border-blue-600 rounded-full cursor-pointer'>Login</button></Link>
+                        <Link to="/cart"><i className="fa-solid fa-cart-shopping px-3 py-1 text-xl text-blue-600 hover:text-blue-400 cursor-pointer"></i></Link>
                     </div>
                 </div>
                 <hr className='mt-3' />
